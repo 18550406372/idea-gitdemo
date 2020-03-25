15619597c94da1813ebf81db62eb64853a457ef4
liuzhao <1146514351@qq.com>
2020-03-25 20:10:09 +0800

    首次提交

---
+++ src/main/java/com/liuzhao/controller/TestController.java
@@ -0,0 +1,12 @@
+package com.liuzhao.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    @RequestMapping("info")
+    public String info(){
+        return "hello gitdemo";
+    }
+}
